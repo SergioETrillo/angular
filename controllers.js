3e83f3904c6edd1f37d90a0dee4036e2a91755f3
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-31 14:27:49 +0100

    add challenge 5

--- controllers.js
+++ controllers.js
@@ -1,6 +1,14 @@
 toDoApp.controller('ToDoController', function(){
   // this.todos = ["ToDo1","TodDo2"];
-    this.todos = [{task: "ToDo1",completed: true},
+  var self = this;
+  self.todos = [{task: "ToDo1",completed: true},
                   {task: "ToDo2", completed: false}];
 
-  });
+  self.addToDo = function(todoText) {
+    self.todos.push({task: todoText, completed: false});
+  };
+
+  self.removeToDo = function() {
+    self.todos.pop();
+  };
+});
